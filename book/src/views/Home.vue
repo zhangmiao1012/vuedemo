9f583acfd3b0336e6403006dc5f0223a944496ef
zmx <429776380@qq.com>
2019-08-30 21:10:32 +0800

    vue的book

---
+++ book/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Book></Book>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Book from './book'
+export default {
+  name: "home",
+  components: {
+    Book
+
+  }
+};
+</script>
+
---
+++ book/src/views/book.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Rita</h1>
+       
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
